29c926e4e4352c1d20965336fb0d90d77c251cc6
ProjectWAW <lukaclasher12832@gmail.com>
2020-04-08 12:57:49 +0200

    meta

---
+++ meta.php
@@ -0,0 +1,21 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content=""> <!-- -->
+<meta name="keywords" content=""> <!-- -->
+<meta property="image" content=""> <!-- -->
+<meta name="theme-color" content=""> <!-- -->
+<meta name="author" content=""> <!-- -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link rel="canonical" href=""> <!-- -->
+<link rel="shortcut icon" type="image/x-icon" href=""> <!-- /images/favicon.ico -->
+<link rel="apple-touch-icon" sizes="57x57" href=""> <!-- /images/apple-touch-icon_57x57.png -->
+<link rel="apple-touch-icon" sizes="76x76" href=""> <!-- /images/apple-touch-icon_76x76.png -->
+<link rel="apple-touch-icon" sizes="120x120" href=""> <!-- /images/apple-touch-icon_120x120.png -->
+<link rel="apple-touch-icon" sizes="152x152" href=""> <!-- /images/apple-touch-icon_152x152.png -->
+<link rel="apple-touch-icon" sizes="167x167" href=""> <!-- /images/apple-touch-icon_167x167.png -->
+<link rel="apple-touch-icon" sizes="180x180" href=""> <!-- /images/apple-touch-icon_180x180.png -->
+<meta property="og:type" content="website">
+<meta property="og:site_name" content=""> <!-- -->
+<meta property="og:description" content=""> <!-- -->
+<meta property="og:image" content=""> <!-- -->
