90657286fa48071ce5fc708c3af872845db6b51e
ProjectWAW <lukaclasher1283@gmail.com>
2020-04-09 17:05:13 +0200

    rest of the countries

---
+++ css.php
@@ -0,0 +1,9 @@
+<script>
+/*if (localStorage.getItem("dark-mode") !== "on") {
+  $('head').append('<link rel="stylesheet" type="text/css" href="/css/all.css">');
+  $('head').append('<link rel="stylesheet" type="text/css" href="/css/navbar-light.css">');
+} else {
+  $('head').append('<link rel="stylesheet" type="text/css" href="/css/dark.css">');
+  $('head').append('<link rel="stylesheet" type="text/css" href="/css/navbar-dark.css">');
+}*/
+</script>
