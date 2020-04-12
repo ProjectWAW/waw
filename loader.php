008fc76146c53a7e83e22fa0f5c2790dee62917e
ProjectWAW <lukaclasher1283@gmail.com>
2020-04-12 18:54:44 +0200

    loader update

--- loader.php
+++ loader.php
@@ -1,5 +1,4 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="css/loader.css">
 <div id="spinner"><div id="loader"></div></div>
 <script>
 $('body,html').css("overflow","hidden");
