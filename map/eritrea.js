2997be7fb7a66e8531f9f0ab54a71d8dff2bac9d
ProjectWAW <lukaclasher1283@gmail.com>
2020-04-15 12:15:18 +0200

    USSR stuff and preparations for italian colonies

---
+++ map/eritrea.js
@@ -0,0 +1,9 @@
+var eritrea = [
+    /* sudan */
+
+    /* ethiopia */
+
+    /* djibouti */
+  ]
+  
+  var eritrea1 = L.polygon(eritrea, {color: 'black'}).addTo(mymap);
---
+++ map/libya.js
@@ -0,0 +1,15 @@
+var libya = [
+  /* tunisia */
+  
+  /* algeria */
+
+  /* french west africa */
+
+  /*french equatorial africa */
+
+  /* egypt */
+
+  /* sudan */
+]
+
+var libya1 = L.polygon(libya, {color: 'black'}).addTo(mymap);
