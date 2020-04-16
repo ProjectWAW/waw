dd863f69ce58ff032e8986831ec52bad38a5dccf
ProjectWAW <lukaclasher1283@gmail.com>
2020-04-16 21:28:03 +0200

    server_map

--- map.php
+++ map.php
@@ -24,6 +24,7 @@
 <body>
 <?php include 'loader.php';
 require 'navbar.php';
+require 'server/server_map.php';
 ?>
 <div id='map' style='width:100%;height:100%;position:absolute;'></div>
 <script>
---
+++ server/server_map.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($_POST['submit'])) {
+  $date = $_POST['date'];
+  if (strpos($date, "1936") === true) {
+    if (strpos($date, "1936/7") === true) {}
+  }
+}
+?>
