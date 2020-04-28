e3a2353263abfe6a8b153354f8e9e3bb26b77ae5
ProjectWAW <lukaclasher1283@gmail.com>
2020-04-28 12:37:19 +0200

    map files

--- map.php
+++ map.php
@@ -2,7 +2,8 @@
 if (isset($_GET['d'])) {
   $date = $_GET['d'];
 } else {
-  $date = "1936.07.16";
+  $date = "1935_10_03";
+  $date_info = "03 October 1935";
 }
 ?>
 <!DOCTYPE html>
@@ -14,13 +15,18 @@ if (isset($_GET['d'])) {
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
 <link rel="stylesheet" href="http://xguaita.github.io/Leaflet.MapCenterCoord/dist/L.Control.MapCenterCoord.min.css"/>
-<link rel="stylesheet" href="https://ppete2.github.io/Leaflet.PolylineMeasure/Leaflet.PolylineMeasure.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+<script src="https://kit.fontawesome.com/02faa02085.js" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
 <script src="http://xguaita.github.io/Leaflet.MapCenterCoord/dist/L.Control.MapCenterCoord.min.js"></script>
 <script src="https://ppete2.github.io/Leaflet.PolylineMeasure/Leaflet.PolylineMeasure.js"></script>
 <script src="http://localhost/waw/js/leaflet.ajax.min.js"></script>
+<link rel="stylesheet" href="server/L.Icon.FontAwesome.css"/>
+<script src="server/L.Icon.FontAwesome.js"></script>
+
 <title>Map - Project: World at War</title>
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
 .navbar {
   position: fixed;
   margin-bottom: 0;
@@ -48,28 +54,94 @@ if (isset($_GET['d'])) {
 .sidebar-links-nav {
   color: #777;
   background-color: #f8f8f8;
-  margin-top: 0;
-  margin-bottom: 0;
+  margin: 0;
+  width: 100%;
+  float: left;
 }
 .sidebar-links {
   border-bottom: 2px solid #e7e7e7;
 }
-/*@media screen and (min-width:1001px) {*/
-  #map {
-    width: 80%;
-    height: 100%;
-    position: absolute;
-  }
-  #sidebar {
-    width: 20%;
-    position: absolute;
-    right: 0;
-    top: 52px;
-  }
-  .sidebar-nav {
-    box-sizing: border-box;
-  }
-@media screen and (max-width: 450px) {
+#map {
+  width: auto;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  right: 399px;
+}
+#sidebar {
+  width: 399px;
+  position: fixed;
+  overflow-y: auto;
+  right: 0;
+  bottom: 0;
+  top: 52px;
+  background-color: white;
+}
+.sidebar-nav {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0px;
+  z-index: 200;
+  box-sizing: border-box;
+}
+.mobile-active {
+  color: dodgerblue;
+  border-bottom: 2px solid dodgerblue;
+}
+.tabcontent {
+  padding: 8px;
+  width: 100%;
+}
+#mobile-nav {
+  display: none;
+}
+.mobile-nav-button {
+  height: 100%;
+  width: 50%;
+  float: left;
+  border: 0;
+  color: #777;
+  background-color: #f8f8f8;
+  border-bottom: 2px solid #e7e7e7;
+}
+.mobile-nav-button:focus, .mobile-nav-button:active {
+  outline: none;
+}
+hr { 
+	height: 1px;
+  background-color: lightgray;
+	border: none;
+  margin-top: 18px;
+  margin-bottom: 18px;
+}
+h4 {
+  text-align: center;
+  margin-top: 60px;
+}
+b {
+  padding-left: 4px;
+  font-size: 20px;
+}
+.info-content {
+  padding-left: 5px;
+  padding-right: 5px;
+  font-size: 17px;
+  font-family: 'Roboto', sans-serif;
+}
+.fas.fa-bullhorn {
+  font-size: 18px;
+}
+.circle-fa {
+  display: inline-block;
+  border-radius: 60px;
+  box-shadow: 0px 0px 2px #888;
+  padding: 0.5em 0.6em;
+}
+.info-clicked {
+  border-left: 2px solid dodgerblue;
+  padding-left: 6px;
+}
+@media screen and (max-width: 767px) {
   #mobile-nav {
     display: block;
     z-index: 400;
@@ -78,27 +150,16 @@ if (isset($_GET['d'])) {
     height: 28px;
     top: 52px;
   }
-  .mobile-nav-button {
-    height: 100%;
-    width: 50%;
-    float: left;
-    border: 0;
-    color: #777;
-    background-color: #f8f8f8;
-    border-bottom: 2px solid #e7e7e7;
-  }
-  .mobile-nav-button:focus, .mobile-nav-button:active {
-    outline: none;
-  }
   #map {
     width: 100%;
     z-index: 200;
+    right: 0;
   }
   #sidebar {
     top: 78px;
+    bottom: 0;
     margin: 0;
     position: fixed;
-    height: 100%;
     width: 100%;
     z-index: 1;
     background-color: white;
@@ -108,7 +169,6 @@ if (isset($_GET['d'])) {
     border-bottom: 2px solid dodgerblue;
   }
 }
-/*}*/
 </style>
 </head>
 <body>
@@ -117,185 +177,241 @@ if (isset($_GET['d'])) {
 require 'navbar.php';
 ?>
 <!--<a href="map.php?d=1936.07.16">s</a>-->
-  <div id="mobile-nav">
-    <button id="showMap" class="mobile-nav-button mobile-active" onClick="showMap()">
-      Map
-    </button>
-    <button id="showSidebar" class="mobile-nav-button" onClick="showSidebar()">
-      Sidebar
-    </button>
-  </div>
-  <div id='map'></div>
-  <!--<div class="sidebar-nav">
-    <ul class="nav navbar-nav">
-      <li><a href="#">Info</a></li>
-      <li><a href="#">Keys</a></li>
-      <li><a href="#">Date</a></li>
+<div id="mobile-nav">
+  <button id="showMap" class="mobile-nav-button mobile-active" onClick="showMap()">Map</button>
+  <button id="showSidebar" class="mobile-nav-button" onClick="showSidebar()">Sidebar</button>
+</div>
+<div id='map'></div>
+<div id="sidebar">
+  <div class="sidebar-nav">
+    <ul class="nav navbar-nav sidebar-links-nav">
+      <li><a id="info-button" style="padding-top:15px;padding-bottom:15px;" class="sidebar-links mobile-active" href="#" onclick="openHelp(event, 'Info');showInfo();" id="defaultOpen">Info</a></li>
+      <li><a id="date-button" style="padding-top:15px;padding-bottom:15px;" class="sidebar-links" href="#" onclick="openHelp(event, 'Date');showDate();">Date</a></li>
+      <li><a id="keys-button" style="padding-top:15px;padding-bottom:15px;" class="sidebar-links" href="#" onclick="openHelp(event, 'Keys');showKeys();">Keys</a></li>
     </ul>
-  </div>-->
-  <div id="sidebar">
-    <div class="sidebar-nav">
-      <ul class="nav navbar-nav sidebar-links-nav">
-        <li><a id="info-button" class="sidebar-links mobile-active" href="#" onclick="openHelp(event, 'Info');showInfo();" id="defaultOpen">Info</a></li>
-        <li><a id="date-button" class="sidebar-links" href="#" onclick="openHelp(event, 'Date');showDate();">Date</a></li>
-        <li><a id="keys-button" class="sidebar-links" href="#" onclick="openHelp(event, 'Keys');showKeys();">Keys</a></li>
-      </ul>
-      <div id="Info" class="tabcontent">
-
-      </div>
-      <div id="Date" class="tabcontent">
-
-      </div>
-      <div id="Keys" class="tabcontent">
-
+  </div>
+  <div id="Info" class="tabcontent">
+    <h4><?php echo $date_info; ?></h4>
+    <hr>
+    <?php include 'map/'.$date.'.php';?>
+    <div>
+      <i class="fas fa-bullhorn circle-fa" onClick="zoom1935_10_03_1()"></i> <b>Germany</b>
+      <div class="info-content">
+        <?php echo $info_1;?> <a href="#">Read more <span class="glyphicon glyphicon-new-window"></span></a>
       </div>
     </div>
+    <hr>
+    <div>
+      <i class="fas fa-bullhorn circle-fa" onClick="zoom1935_10_03_1()"></i> <b>Germany</b>
+      <div class="info-content">
+        <?php echo $info_1;?> <a href="#">Read more <span class="glyphicon glyphicon-new-window"></span></a>
+      </div>
+    </div>
+    <hr>
+    <div>
+      <i class="fas fa-bullhorn circle-fa" onClick="zoom1935_10_03_1()"></i> <b>Germany</b>
+      <div class="info-content">
+        <?php echo $info_1;?> <a href="#">Read more <span class="glyphicon glyphicon-new-window"></span></a>
+      </div>
+    </div>
+    <hr>
+    <div>
+      <i class="fas fa-bullhorn circle-fa" onClick="zoom1935_10_03_1()"></i> <b>Germany</b>
+      <div class="info-content">
+        <?php echo $info_1;?> <a href="#">Read more <span class="glyphicon glyphicon-new-window"></span></a>
+      </div>
+    </div>
+    <hr>
+    <div>
+      <i class="fas fa-bullhorn circle-fa" onClick="zoom1935_10_03_1()"></i> <b>Germany</b>
+      <div class="info-content">
+        <?php echo $info_1;?> <a href="#">Read more <span class="glyphicon glyphicon-new-window"></span></a>
+      </div>
+    </div>
+    <hr>
+    <div id="test2">
+      <i class="fas fa-bullhorn circle-fa" onClick="zoom1935_10_03_1()"></i> <b>Germany</b>
+      <div class="info-content">
+        <?php echo $info_1;?> <a href="#">Read more <span class="glyphicon glyphicon-new-window"></span></a>
+      </div>
+    </div>
+    <hr>
   </div>
-  <script>
-    var mymap = L.map('map').setView([58.229553, -6.206524], 9);
+  <div id="Date" class="tabcontent" style="display:none">
+  <div class="date-selection">
+    <div class="year">1935</div>
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</a>',
-      minZoom: 3,
-      maxZoom: 14,
-      zoomControl: true
-    }).addTo(mymap);
+    <div class="year">1936</div>
 
-    function add_geojson_layer(name, color="red") {
+    <div class="year">1937</div>
 
-      var ll = new L.GeoJSON.AJAX("geojson_files/1935_10_02/"+name+".geojson");
+    <div class="year">1938</div>
 
-    ll.on('data:loaded', function() {
-      ll.setStyle({
-        color: color
-      });
-      ll.addTo(mymap);
+    <div class="year">1939</div>
+
+    <div class="year">1940</div>
+
+    <div class="year">1941</div>
+
+    <div class="year">1942</div>
+
+    <div class="year">1943</div>
+
+    <div class="year">1944</div>
+
+    <div class="year">1945</div>
+  </div>
+  </div>
+  <div id="Keys" class="tabcontent" style="display:none">
+a   
+  </div>
+</div>
+<script>
+  var mymap = L.map('map').setView([58.229553, -6.206524], 9);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</a>',
+    minZoom: 3,
+    maxZoom: 14,
+    zoomControl: true
+  }).addTo(mymap);
+
+  function add_geojson_layer(name, color) {
+
+  var ll = new L.GeoJSON.AJAX("geojson_files/1935_10_03/"+name+".geojson");
+
+  ll.on('data:loaded', function() {
+    ll.setStyle({
+      color: color
     });
-    }
+    ll.addTo(mymap);
+  });
+  }
 
-    /***** COLORS *****/
-    var axis = 'black'
-    var axis_puppet = '#666666'
-    var axis_occupied = '#a1a1a1'
+  /***** COLORS *****/
+  var axis = 'black'
+  var axis_puppet = '#666666'
+  var axis_occupied = '#a1a1a1'
 
-    var allies = '#296d98'
-    var allies_puppet = '#3792cb'
-    var allies_occupied = '#45b6fe'
+  var allies = '#296d98'
+  var allies_puppet = '#3792cb'
+  var allies_occupied = '#45b6fe'
 
-    var comintern = '#B30000'
-    var comintern_puppet = 'red'
-    var comintern_occupied = '#ff7f7f'
+  var comintern = '#B30000'
+  var comintern_puppet = 'red'
+  var comintern_occupied = '#ff7f7f'
 
-    var finland = 'purple'
-    var finland_occupied = '#ac68cc'
+  var finland = 'purple'
+  var finland_occupied = '#ac68cc'
 
-    var neutral = '#ffca8a'
-    var neutral_zone = 'white'
+  var neutral = '#ffca8a'
+  var neutral_zone = 'white'
 
+  var countries = [
+    ["neutral_zone_iraq", neutral_zone],
 
-    var countries = [
-      ["neutral_zone_iraq", neutral_zone],
+    ["andorra", neutral],
+    ["albania", neutral],
+    ["bhutan", neutral],
+    ["czechoslovakia", neutral],
+    ["finland", neutral],
+    ["danzig", neutral],
+    ["greece", neutral],
+    ["iraq", neutral],
+    ["lichtenstein", neutral],
+    ["luxembourg", neutral],
+    ["poland", neutral],
+    ["portugal", neutral],
+    ["romania", neutral],
+    ["spanish_africa", neutral],
+    ["spanish_sahara", neutral],
+    ["turkey", neutral],
+    ["yugoslavia", neutral],
 
-      ["andorra", neutral],
-      ["albania", neutral],
-      ["bhutan", neutral],
-      ["czechoslovakia", neutral],
-      ["finland", neutral],
-      ["danzig", neutral],
-      ["greece", neutral],
-      ["iraq", neutral],
-      ["lichtenstein", neutral],
-      ["luxembourg", neutral],
-      ["poland", neutral],
-      ["portugal", neutral],
-      ["romania", neutral],
-      ["spanish_africa", neutral],
-      ["spanish_sahara", neutral],
-      ["turkey", neutral],
-      ["yugoslavia", neutral],
+    ["bahrain", allies_puppet],
+    ["bermuda", allies_puppet],
+    ["british_africa", allies_puppet],
+    ["british_somaliland", allies_puppet],
+    ["cyprus", allies_puppet],
+    ["djibouti", allies_puppet],
+    ["france", allies],
+    ["french_africa", allies_puppet],
+    ["french_syria", allies_puppet],
+    ["gambia", allies_puppet],
+    ["gibraltar", allies_puppet],
+    ["kuwait", allies_puppet],
+    ["malta", allies_puppet],
+    ["qatar", allies_puppet],
+    ["south_georgia", allies_puppet],
+    ["swaziland", allies_puppet],
+    ["uk", allies],
 
-      ["bahrain", allies_puppet],
-      ["bermuda", allies_puppet],
-      ["british_africa", allies_puppet],
-      ["british_somaliland", allies_puppet],
-      ["cyprus", allies_puppet],
-      ["djibouti", allies_puppet],
-      ["france", allies],
-      ["french_africa", allies_puppet],
-      ["french_syria", allies_puppet],
-      ["gambia", allies_puppet],
-      ["gibraltar", allies_puppet],
-      ["kuwait", allies_puppet],
-      ["malta", allies_puppet],
-      ["qatar", allies_puppet],
-      ["south_georgia", allies_puppet],
-      ["swaziland", allies_puppet],
-      ["uk", allies],
+    ["mongolia", comintern_puppet],
+    ["tannu_tuva", comintern_puppet],
+    ["ussr", comintern],
 
-      ["mongolia", comintern_puppet],
-      ["tannu_tuva", comintern_puppet],
-      ["ussr", comintern],
+    ["eritrea", axis_puppet],
+    ["german_prussia", axis],
+    ["germany", axis],
+    ["italian_dodecanese", axis],
+    ["libya", axis_puppet],
+    ["zara", axis]
+  ]
 
-      ["eritrea", axis_puppet],
-      ["german_prussia", axis],
-      ["germany", axis],
-      ["italian_dodecanese", axis],
-      ["libya", axis_puppet],
-      ["zara", axis]
-    ]
+  for (country of countries) {
+    add_geojson_layer(country[0], country[1]);
+  }
 
-    for (country of countries) {
-      add_geojson_layer(country[0], country[1]);
-    }
+<?php include 'map_markers/'.$date.'.js'; ?>
 
-    L.control.mapCenterCoord({
-      icon: false,
-      position: 'bottomright',
-      latlngFormat: 'DMS'
-    }).addTo(mymap);
+  L.control.mapCenterCoord({
+    icon: false,
+    position: 'bottomright',
+    latlngFormat: 'DMS'
+  }).addTo(mymap);
 
-    L.control.scale({
-      position: 'bottomright'
-    }).addTo(mymap);
+  L.control.scale({
+    position: 'bottomright'
+  }).addTo(mymap);
 
-    mymap.zoomControl.setPosition('bottomleft');
+  mymap.zoomControl.setPosition('bottomleft');
+
+  L.control.polylineMeasure({
+    position: 'bottomleft'
+  }).addTo(mymap);
 
     /*var myButtonOptions = {
-      'text': 'MyButton',  // string
-      'iconUrl': 'radar2.png',  // string
-      'onClick': closeIcons,  // callback function
-      'hideText': true,  // bool
-      'maxWidth': 30,  // number
-      'doToggle': false,  // bool
-      'toggleStatus': false  // bool
-    }   
+    'text': 'MyButton',  // string
+    'iconUrl': 'radar2.png',  // string
+    'onClick': closeIcons,  // callback function
+    'hideText': true,  // bool
+    'maxWidth': 30,  // number
+    'doToggle': false,  // bool
+    'toggleStatus': false  // bool
+  }   
 
-    var myButton = new L.Control.Button(myButtonOptions).addTo(mymap);*/
+  var myButton = new L.Control.Button(myButtonOptions).addTo(mymap);*/
 
-    L.control.polylineMeasure({
-      position: 'bottomleft'
-    }).addTo(mymap);
+  var southWest = L.latLng(-90, -180), northEast = L.latLng(90, 180);
+  var bounds = L.latLngBounds(southWest, northEast);
 
-    var southWest = L.latLng(-90, -180), northEast = L.latLng(90, 180);
-    var bounds = L.latLngBounds(southWest, northEast);
+  mymap.setMaxBounds(bounds);
 
-    mymap.setMaxBounds(bounds);
+  mymap.on('drag', function() {
+    mymap.panInsideBounds(bounds, { animate: false });
+  });
+  var popup = L.popup();
 
-    mymap.on('drag', function() {
-	    mymap.panInsideBounds(bounds, { animate: false });
-    });
-    var popup = L.popup();
+  function onMapClick(e) {
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(mymap);
+  }
 
-    function onMapClick(e) {
-      popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-    }
+  mymap.on('click', onMapClick);
 
-    mymap.on('click', onMapClick);
-
-    var radar = L.icon({
+  var radar = L.icon({
     iconUrl: 'radar2.png',
     iconSize: [20, 20],
     iconAnchor: [20, 20],
@@ -303,12 +419,9 @@ require 'navbar.php';
     /*shadowUrl: 'my-icon-shadow.png',*/
     shadowSize: [20, 20],
     shadowAnchor: [20, 20]
-});
+  });
 
 /*var radars = L.layerGroup([]);*/
-
-</script>
-<script>
 
 var elem = document.documentElement;
 function hideSidebar() {
@@ -316,11 +429,11 @@ function hideSidebar() {
   var leafletmap = document.getElementById('map');
   if (sidebar.style.display === "none") {
     sidebar.style.display = "block";
-    leafletmap.style.width = "80%";
+    leafletmap.style.right = "399px";
     mymap.invalidateSize();
   } else {
     sidebar.style.display = "none";
-    leafletmap.style.width = "100%";
+    leafletmap.style.right= "0";
     mymap.invalidateSize();
   }
 }
@@ -411,7 +524,6 @@ function openHelp(evt, contentName) {
   document.getElementById(contentName).style.display = "block";
   evt.currentTarget.className += " active";
 }
-document.getElementById("defaultOpen").click();
 /*function closeIcons() {
     event.preventDefault();
     if(map.hasLayer(radars)) {
