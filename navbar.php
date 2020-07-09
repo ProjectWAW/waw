1279f00a21de3e17d446fd3f4f70f1be9a864311
ProjectWAW <projectworldatwar@gmail.com>
2020-07-09 18:04:10 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -6,22 +6,22 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="/index.php">WebSiteName</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav mr-auto">
-        <li><a href="/index.php">Map</a></li>
-        <li><a href="/editor.php">Editor</a></li>
+        <li><a href="/index.php">(Dev build) Map</a></li>
+        <li><a href="/editor.php">(Dev build) Editor</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <?php if($_SERVER['PHP_SELF']=='/index.php') {
           //echo '<li><a href="/settings.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>';
-          echo '<li id="toggleSidebar"><a href="#" onclick="hideSidebar()"><span class="glyphicon glyphicon-align-right"></span> Toggle Sidebar</a></li>';
+          echo '<li id="toggleSidebar"><a href="#" onclick="hideSidebar()"><span class="glyphicon glyphicon-align-right"></span>&nbsp;&nbsp;Toggle Sidebar</a></li>';
           $iPod = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
           $iPhone = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
           $iPad = stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
           if ($iPod == false && $iPhone == false && $iPad == false) { 
-            echo '<li><a href="#" onclick="openFullscreen()"><span class="glyphicon glyphicon-fullscreen"></span> Full screen</a></li>';
+            echo '<li><a href="#" onclick="openFullscreen()"><span class="glyphicon glyphicon-fullscreen"></span>&nbsp;&nbsp;Full screen</a></li>';
           }
         }?>
       </ul>
