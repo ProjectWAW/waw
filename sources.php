ada14855f79da18b6b1744658c80a52e1ea8c908
ProjectWAW <projectworldatwar@gmail.com>
2020-08-08 21:12:55 +0200

    Sources page added + SCW progress

---
+++ sources.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<?php include 'page_head.php';?>
+
+<title>Map Sources - Project: World at War</title>
+<style>
+
+</style>
+</head>
+<body>
+<?php include 'loader.php';
+require 'navbar.php';
+?>
+
+
+
+</body>
+</html>
