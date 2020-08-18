34ef8987200170bd4954f461a8a6faa160fa7251
ProjectWAW <projectworldatwar@gmail.com>
2020-08-18 16:16:06 +0200

    SCW progress

--- editor2.php
+++ editor2.php
@@ -404,7 +404,7 @@ function repositionImage() {
       country = val;
       console.log(country);
 
-      var geojsonLayer = new L.GeoJSON.AJAX("geojson_files/1936_08_02/" + country + ".geojson");
+      var geojsonLayer = new L.GeoJSON.AJAX("geojson_files/1936_08_12/" + country + ".geojson");
 
       geojsonLayer.on('data:loaded', function() {
         geojsonLayer.addTo(mymap);
