89fa24087a40e7acbba772d96b292fd4eab07b09
ProjectWAW <projectworldatwar@gmail.com>
2020-09-02 12:41:03 +0200

    Delete previous cookies on date change to avoid too big headers

--- index.php
+++ index.php
@@ -60,25 +60,16 @@ function getCookie(name) {
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
 }
-function clearListCookies()
-{   
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++)
-    {   
-        var spcook =  cookies[i].split("=");
-        deleteCookie(spcook[0]);
-    }
-    function deleteCookie(cookiename)
-    {
-        var d = new Date();
-        d.setDate(d.getDate() - 1);
-        var expires = ";expires="+d;
-        var name=cookiename;
-        //alert(name);
-        var value="";
-        document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
-    }
-}
+function clearListCookies() { 
+                var allCookies = document.cookie.split(';'); 
+                
+                // The "expire" attribute of every cookie is  
+                // Set to "Thu, 01 Jan 1970 00:00:00 GMT" 
+                for (var i = 0; i < allCookies.length; i++) 
+                    document.cookie = allCookies[i] + "=;expires=" 
+                    + new Date(0).toUTCString();
+  
+            } 
 clearListCookies();
 </script>
 <script src="https://kit.fontawesome.com/02faa02085.js"></script>
@@ -1124,11 +1115,11 @@ $(function() {
           mymap.addLayer(country_layers);
         });
       }
-      /*if (number == 2) {
+      if (number == 2) {
         marker_group.remove();
       } else {
         marker_group.addTo(mymap);
-      }*/
+      }
     });
   });
 });
@@ -1301,11 +1292,11 @@ $(function() {
           mymap.addLayer(country_layers);
         });
       }
-      /*if (number == 2) {
+      if (number == 2) {
         marker_group.remove();
       } else {
         marker_group.addTo(mymap);
-      }*/
+      }
     });
   });
 });
