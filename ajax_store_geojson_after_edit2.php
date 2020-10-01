bebe5abeefe0bb5b9357d1b0944c4204ba7c1c32
ProjectWAW <projectworldatwar@gmail.com>
2020-10-01 09:34:28 +0200

    SCW progress mid-November

--- ajax_store_geojson_after_edit2.php
+++ ajax_store_geojson_after_edit2.php
@@ -1,6 +1,6 @@
 <?php
 $geojsondata = $_POST['gdata'];
 $country = $_POST['country'];
-$file = "geojson_files/1936_10_17/$country.geojson";
+$file = "geojson_files/1936_11_13/$country.geojson";
 file_put_contents($file, $geojsondata);
 ?>
