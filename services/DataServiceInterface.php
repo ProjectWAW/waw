8dc0cb21dc2e7f844cc2ef066935b0b608b62123
Lord Drake <jsparn@gmail.com>
2020-10-07 09:54:43 -0400

    Added missing method

--- services/DataServiceInterface.php
+++ services/DataServiceInterface.php
@@ -18,6 +18,7 @@
         public function GetConflict(string $id): array;
         public function GetAllEvents(): array;
         public function GetEvent(string $id): array;
+        public function GetEventsByDate(string $date): array;
         public function GetAllMarkers(): array;
         public function GetMarker(string $id): array;
         public function GetAllNations(): array;
