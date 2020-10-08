5f77eb3c95f85c06f114b48888df67628bae4460
Lord Drake <jsparn@gmail.com>
2020-10-08 15:59:21 -0400

    Added MapEventsService

--- services/DataService.php
+++ services/DataService.php
@@ -1,7 +1,6 @@
 <?php
 
     include('DataServiceInterface.php');
-    include('../models/Conflict.php');
 
     // TODO: Convert to a smaller services
     abstract class DataService implements DataServiceInterface
@@ -50,94 +49,5 @@
             }
 
             return null;
-        }
-
-        /**
-         * Add new Event to the map_events table
-         *
-         * @param string $date
-         * @param string $markerID
-         * @param array $location
-         * @param string $text
-         * @param string $cssClass
-         * @param string $conflictID
-         * @param string $countryID
-         * @param string $sourceID
-         */
-        public function AddEvent(
-          string $date,
-          string $markerID,
-          array $location,
-          string $text,
-          string $cssClass,
-          string $conflictID,
-          string $countryID,
-          string $sourceID
-        ): void {
-            try
-            {
-                $conn = $this->TryConnect();
-
-                if (!$conn)
-                {
-                    throw new RuntimeException("Database connection cannot be null");
-                }
-
-                $statement = $conn->prepare("INSERT INTO map_events (
-                        id, date, marker, location, text, css_class, conflict, country, source
-                        ) VALUES (:id, :date, :marker, :location, :text, :css_class, :conflict, :country, :source)");
-
-                $id = uniqid('', true);
-
-                $statement->bindParam(':id', $id);
-                $statement->bindParam('date', $date);
-                $statement->bindParam('marker', $markerID);
-                $statement->bindParam('location', $location);
-                $statement->bindParam('text', $text);
-                $statement->bindParam('css_class', $cssClass);
-                $statement->bindParam('conflict', $conflictID);
-                $statement->bindParam('country', $countryID);
-                $statement->bindParam('source', $sourceID);
-                $statement->execute();
-            }
-            catch (Exception $e)
-            {
-                // log error
-                echo "Adding Event failed: " . $e->getMessage();
-            }
-        }
-
-        /**
-         * Gets all Events from the map_events table
-         *
-         * @return array
-         */
-        public function GetAllEvents(): array
-        {
-            // TODO: Implement GetAllEvents() method.
-        }
-
-        /**
-         * Gets an Event by ID from the map_events table
-         *
-         * @param string $id
-         *
-         * @return array
-         */
-        public function GetEvent(string $id): array
-        {
-            // TODO: Implement GetEvent() method.
-        }
-
-        /**
-         * Gets an array of Events by Date from the map_events table
-         *
-         * @param string $date
-         *
-         * @return array
-         */
-        public function GetEventsByDate(string $date): array
-        {
-            // TODO: Implement GetEventsByDate() method.
         }
     }
--- services/DataServiceInterface.php
+++ services/DataServiceInterface.php
@@ -7,16 +7,16 @@
 
 //        public function AddAuthor(string $name): void;
 //        public function AddConflict(string $name): void;
-        public function AddEvent(
-          string $date,
-          string $markerID,
-          array $location,
-          string $text,
-          string $cssClass,
-          string $conflictID,
-          string $countryID,
-          string $sourceID
-        ): void;
+//        public function AddEvent(
+//          string $date,
+//          string $markerID,
+//          array $location,
+//          string $text,
+//          string $cssClass,
+//          string $conflictID,
+//          string $countryID,
+//          string $sourceID
+//        ): void;
 //        public function AddMarker(string $name): void;
 //        public function AddCountry(
 //          string $name,
@@ -32,9 +32,9 @@
 //        public function GetAuthor(string $id): array;
 //        public function GetAllConflicts(): array;
 //        public function GetConflict(string $id): array;
-        public function GetAllEvents(): array;
-        public function GetEvent(string $id): array;
-        public function GetEventsByDate(string $date): array;
+//        public function GetAllEvents(): array;
+//        public function GetEvent(string $id): array;
+//        public function GetEventsByDate(string $date): array;
 //        public function GetAllMarkers(): array;
 //        public function GetMarker(string $id): array;
 //        public function GetAllCountries(): array;
