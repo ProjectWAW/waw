46395ab68b356fb5837d1caed42119e177218d1b
Lord Drake <jsparn@gmail.com>
2020-10-12 19:35:22 -0400

    Squashed line length

--- config.php
+++ config.php
@@ -4,5 +4,10 @@
     define("SITE_ROOT", __DIR__);
 
     // DB Settings
-    $settings = json_decode(file_get_contents(SITE_ROOT . "/config.json"), true);
+    $settings = json_decode(
+      file_get_contents(SITE_ROOT . "/config.json"),
+      true,
+      512,
+      JSON_THROW_ON_ERROR
+    );
     define("DB", $settings);
