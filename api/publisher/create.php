0d0dab51f04e2821a8053a5cb2f040eb235681a8
Lord Drake <jsparn@gmail.com>
2020-10-12 19:32:31 -0400

    Added publisher create endpoint

---
+++ api/publisher/create.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once __DIR__ . "/../../config.php";
+    require_once SITE_ROOT . "/services/PublishersService.php";
+
+    // Headers
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: "
+        . "Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
+
+    try
+    {
+        $data =
+          json_decode(file_get_contents("php://input"), true, 512, JSON_THROW_ON_ERROR);
+
+        $service = new PublishersService();
+        $service->AddPublisher($data["name"]);
+
+        http_response_code(204);
+    }
+    catch (Exception $e)
+    {
+        http_response_code(500);
+        echo json_encode($e->getMessage(), JSON_THROW_ON_ERROR);
+    }
