306c3258f97a3121efd3f5faee18de61b3c56380
Lord Drake <jsparn@gmail.com>
2020-10-13 20:55:23 -0400

    Tweaked db scripts

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -4,32 +4,32 @@ USE waw;
 
 CREATE TABLE source_types
 (
-    id   varchar(255) NOT NULL,
+    id   varchar(36) NOT NULL,
     type varchar(16)  NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE authors
 (
-    id   varchar(255) NOT NULL,
+    id   varchar(36) NOT NULL,
     name varchar(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE publishers
 (
-    id   varchar(255) NOT NULL,
+    id   varchar(36) NOT NULL,
     name varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE sources
 (
-    id        varchar(255) NOT NULL,
-    type      varchar(255) NOT NULL,
-    author    varchar(255),
+    id        varchar(36) NOT NULL,
+    type      varchar(36) NOT NULL,
+    author    varchar(36),
     title     varchar(255) NOT NULL,
-    publisher varchar(255) NOT NULL,
+    publisher varchar(36) NOT NULL,
     date      date,
     PRIMARY KEY (id),
     FOREIGN KEY (type) REFERENCES source_types (id),
@@ -39,7 +39,7 @@ CREATE TABLE sources
 
 CREATE TABLE countries
 (
-    id                 varchar(255) NOT NULL,
+    id                 varchar(36) NOT NULL,
     name               varchar(255) NOT NULL,
     status             varchar(255) NOT NULL,
     government         varchar(150) NOT NULL,
@@ -50,29 +50,29 @@ CREATE TABLE countries
 
 CREATE TABLE conflicts
 (
-    id   varchar(255) NOT NULL,
+    id   varchar(36) NOT NULL,
     name varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE markers
 (
-    id   varchar(255) NOT NULL,
+    id   varchar(36) NOT NULL,
     name varchar(30)  NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE map_events
 (
-    id        varchar(255) NOT NULL,
+    id        varchar(36) NOT NULL,
     date      date         NOT NULL,
-    marker    varchar(255) NOT NULL,
+    marker    varchar(36) NOT NULL,
     location  varchar(30)  NOT NULL,
     text      text         NOT NULL,
     css_class varchar(30)  NOT NULL,
-    conflict  varchar(255) NOT NULL,
-    country   varchar(255) NOT NULL,
-    source    varchar(255) NOT NULL,
+    conflict  varchar(36) NOT NULL,
+    country   varchar(36) NOT NULL,
+    source    varchar(36) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (marker) REFERENCES markers (id),
     FOREIGN KEY (conflict) REFERENCES conflicts (id),
--- sql/populateTestData.sql
+++ sql/populateTestData.sql
@@ -3,36 +3,46 @@ USE waw;
 START TRANSACTION;
 
 INSERT INTO source_types (id, type)
-VALUES ('1', 'book'),
-       ('2', 'web');
+VALUES ('0397fd09-ebeb-46b1-9ff9-04589c8e4df8', 'book'),
+       ('099526e6-1a2c-4f4e-b8d3-6306cce25140', 'web');
 
 INSERT INTO authors (id, name)
-VALUES ('1', 'Bob Merkal'),
-       ('2', 'Harriet Jones');
+VALUES ('10f0bea5-6fb8-492a-a0a5-1cfda571f78f', 'Bob Merkal'),
+       ('2595197f-76f4-4078-acae-cd4ce68d4e5a', 'Harriet Jones');
 
 INSERT INTO publishers (id, name)
-VALUES ('1', 'Gold Leaf Publishing'),
-       ('2', 'Little House');
+VALUES ('2f8cb15e-2c8d-4d8c-b09d-2963dc84562d', 'Gold Leaf Publishing'),
+       ('5f586c64-c45c-44d1-9779-96109087dd3d', 'Little House');
 
 INSERT INTO sources (id, type, author, title, publisher, date)
-VALUES ('1', '2', '2', 'Secret Bob Ross: 2020', '1', '2012-6-28'),
-       ('2', '1', '1', 'War in the East', '2', '1980-01-01');
+VALUES ('88fdb0a9-f81f-42d6-ad49-fdb8df0cfa74', '099526e6-1a2c-4f4e-b8d3-6306cce25140',
+        '2595197f-76f4-4078-acae-cd4ce68d4e5a', 'Secret Bob Ross: 2020', '2f8cb15e-2c8d-4d8c-b09d-2963dc84562d',
+        '2012-6-28'),
+       ('892de04a-2dbf-4cc3-bb07-e4ca08834b8c', '0397fd09-ebeb-46b1-9ff9-04589c8e4df8',
+        '10f0bea5-6fb8-492a-a0a5-1cfda571f78f', 'War in the East', '5f586c64-c45c-44d1-9779-96109087dd3d',
+        '1980-01-01');
 
 INSERT INTO countries (id, name, status, government, party, head_of_government)
-VALUES ('1', 'Boblandia', 'Benevolent Dictatorship?', 'Hippie Commune', 'Pollenators United Front', 'Bob Ross'),
-       ('2', 'Spanish State', 'Nationalist faction in the Spanish civil war', 'Unitary personalist dictatorship',
+VALUES ('8eb1c021-4151-44e3-9cda-553683297a0f', 'Boblandia', 'Benevolent Dictatorship?', 'Hippie Commune',
+        'Pollenators United Front', 'Bob Ross'),
+       ('a5b853f7-88b1-4974-9ef0-eda4216dd4d9', 'Spanish State', 'Nationalist faction in the Spanish civil war',
+        'Unitary personalist dictatorship',
         'National Movement (Movimiento Nacional)', 'Francisco Franco');
 
 INSERT INTO conflicts (id, name)
-VALUES ('1', 'Pollenist Rebellion'),
-       ('2', 'Spanish Civil War');
+VALUES ('ad56fd89-b1e4-46d0-8d93-8092b83b6240', 'Pollenist Rebellion'),
+       ('bcbdc9c0-5954-4076-9398-f133f06ddb9f', 'Spanish Civil War');
 
 INSERT INTO markers (id, name)
-VALUES ('1', 'green_bullhorn'),
-       ('2', 'green-gun-right');
+VALUES ('cb867ccc-d8ee-42ac-8937-77f67018c458', 'green_bullhorn'),
+       ('ce4d7026-1b66-4f6a-a76d-db5585cc48a1', 'green-gun-right');
 
 INSERT INTO map_events (id, date, marker, location, text, css_class, conflict, country, source)
-VALUES ('1', DATE('1936_10_2'), '1', '14.495184,39.393883', 'BEEEEEEEEEEESSSS', 'icon-gun-right', '1', '1', '1'),
-       ('2', DATE('1936_10_2'), '2', '5.190791,45.079651', 'Bang bang, boom boom', 'icon-gun-right', '2', '2', '2');
+VALUES ('d3d94ed6-973d-4ba6-bccc-a73fe5410a00', DATE('1936_10_2'), 'cb867ccc-d8ee-42ac-8937-77f67018c458',
+        '14.495184,39.393883', 'BEEEEEEEEEEESSSS', 'icon-gun-right', 'ad56fd89-b1e4-46d0-8d93-8092b83b6240',
+        '8eb1c021-4151-44e3-9cda-553683297a0f', '88fdb0a9-f81f-42d6-ad49-fdb8df0cfa74'),
+       ('d61790e8-f171-4002-a455-1da9cda6c787', DATE('1936_10_2'), 'ce4d7026-1b66-4f6a-a76d-db5585cc48a1',
+        '5.190791,45.079651', 'Bang bang, boom boom', 'icon-gun-right', 'bcbdc9c0-5954-4076-9398-f133f06ddb9f',
+        'a5b853f7-88b1-4974-9ef0-eda4216dd4d9', '892de04a-2dbf-4cc3-bb07-e4ca08834b8c');
 
 COMMIT;
