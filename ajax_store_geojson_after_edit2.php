c1fe1c76410cd42fd769d5d6cb5d238589755b47
ProjectWAW <projectworldatwar@gmail.com>
2020-10-24 18:28:07 +0200

    Vizcaya campaign beginning

--- ajax_store_geojson_after_edit2.php
+++ ajax_store_geojson_after_edit2.php
@@ -1,6 +1,6 @@
 <?php
 $geojsondata = $_POST['gdata'];
 $country = $_POST['country'];
-$file = "geojson_files/1937_04_21/$country.geojson";
+$file = "geojson_files/1937_04_24/$country.geojson";
 file_put_contents($file, $geojsondata);
 ?>
