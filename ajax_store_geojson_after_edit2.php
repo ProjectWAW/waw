5df1383ed9de9cf45c64077f154e9508f0d9a224
ProjectWAW <projectworldatwar@gmail.com>
2020-11-20 10:03:46 +0100

    Campaign of Vizcaya progess

--- ajax_store_geojson_after_edit2.php
+++ ajax_store_geojson_after_edit2.php
@@ -1,6 +1,6 @@
 <?php
 $geojsondata = $_POST['gdata'];
 $country = $_POST['country'];
-$file = "geojson_files/1937_06_11/$country.geojson";
+$file = "geojson_files/1937_06_13/$country.geojson";
 file_put_contents($file, $geojsondata);
 ?>
