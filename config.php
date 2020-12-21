ba8e62843e739d1fa4365f93967b36ae717a32b6
Lord Drake <jsparn@gmail.com>
2020-12-21 09:48:11 -0500

    Added catch for failing to load config file

--- config.php
+++ config.php
@@ -4,10 +4,17 @@
     define("SITE_ROOT", __DIR__);
 
     // DB Settings
-    $settings = json_decode(
-      file_get_contents(SITE_ROOT . "/config.json"),
-      true,
-      512,
-      JSON_THROW_ON_ERROR
-    );
-    define("DB", $settings);
+    try {
+        $settings = json_decode(
+          file_get_contents(SITE_ROOT . "/config.json"),
+          true,
+          512,
+          JSON_THROW_ON_ERROR
+        );
+
+        define("config", $settings);
+    } catch (JsonException $e) {
+        echo "Failed to load config.json: " . $e->getMessage();
+    }
+
+
