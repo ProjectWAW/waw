14db8d6c2a2ea3be8e974afa17de1c917b5e656f
Lord Drake <jsparn@gmail.com>
2020-12-23 22:58:41 -0500

    Fixed broken type mapping

--- models/Source.php
+++ models/Source.php
@@ -1,13 +1,11 @@
 <?php
 
-    use Ramsey\Uuid\UuidInterface;
-
     class Source
     {
         public string $_id;
         public string $author;
-        public DateTime $accessed;
-        public DateTime $publishDate;
+        public string $accessed;
+        public string $publishDate;
         public string $publisher;
         public string $title;
         public string $type;
