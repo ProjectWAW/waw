5147bfb2cc2f3ea53bf34988f80e4cacd92a4a5b
Lord Drake <jsparn@gmail.com>
2021-01-14 15:31:29 -0500

    Updated action marker script to use a promise

--- action_marker_editor.php
+++ action_marker_editor.php
@@ -1,15 +1,30 @@
 <?php
 if (isset($_POST['submit1'])) {
-  echo '<script>
-  $.ajax({
-    type: "POST", url: "api/countries/create.php", dataType:"json", data: {name: "test1"},
-    success: function(data) {
-    },
-    error:function (xhr, ajaxOptions, thrownError) {
-      document.write(xhr.status);
-      document.write(thrownError);
-    }
-  });
-  </script>';
+ echo "<script>
+  function AjaxCall() {
+    return new Promise(function (resolve, reject) {
+      const objXMLHttpRequest = new XMLHttpRequest();
+
+      objXMLHttpRequest.onreadystatechange = function () {
+        if (objXMLHttpRequest.readyState === 4) {
+          if (objXMLHttpRequest.status === 200) {
+            resolve(objXMLHttpRequest.responseText);
+          } else {
+            reject('Error Code: ' + objXMLHttpRequest.status + ' Error Message: ' + objXMLHttpRequest.statusText);
+          }
+        }
+      };
+
+      objXMLHttpRequest.open('POST', 'api/countries/create.php');
+      objXMLHttpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      objXMLHttpRequest.send('{\"government\": \"Da gobment\",\"headOfGovernment\": \"Sample\",\"name\": \"Boop\",\"party\": \"yadda yadda\",\"status\": \"Booping\"}');
+    });
+  }
+
+  AjaxCall().then(
+    data => { console.log('Success Response: ' + data) },
+    error => { console.log(error) }
+  );
+  </script>";
 }
 ?>
